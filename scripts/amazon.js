9a20b5f155a20f2174da3dac30fc579206fdf401
tesfu3k <tesfaye4r@gmail.com>
2025-08-02 19:59:11 +0300

    Generate HTML with JavaScript

---
+++ scripts/amazon.js
@@ -0,0 +1,83 @@
+const products = [
+  {
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    priceCents: 1090,
+  },
+  {
+    image: "images/products/intermediate-composite-basketball.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127,
+    },
+    priceCents: 2095,
+  },
+  {
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 56,
+    },
+    priceCents: 799,
+  },
+];
+
+let productsHTML = "";
+
+products.forEach((products) => {
+  productsHTML += `<div class="product-container" bis_skin_checked="1">
+          <div class="product-image-container" bis_skin_checked="1">
+            <img class="product-image" src="${products.image}">
+          </div>
+
+          <div class="product-name limit-text-to-2-lines" bis_skin_checked="1">
+            ${products.name}
+          </div>
+
+          <div class="product-rating-container" bis_skin_checked="1">
+            <img class="product-rating-stars" src="images/ratings/rating-${
+              products.rating.stars * 10
+            }.png">
+            <div class="product-rating-count link-primary" bis_skin_checked="1">${
+              products.rating.count
+            }</div>
+          </div>
+s
+          <div class="product-price" bis_skin_checked="1">${(
+            products.priceCents / 100
+          ).toFixed(2)}</div>
+
+          <div class="product-quantity-container" bis_skin_checked="1">
+            <select>
+              <option selected="" value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer" bis_skin_checked="1"></div>
+
+          <div class="added-to-cart" bis_skin_checked="1">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary">Add to Cart</button>
+        </div>`;
+});
+console.log(productsHTML);
+
+document.querySelector(".js-products-grid").innerHTML = productsHTML;
