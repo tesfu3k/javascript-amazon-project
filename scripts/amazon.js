1c087d1a3593f808c6085cd43c8d2408135652c5
tesfu3k <tesfaye4r@gmail.com>
2025-08-04 09:53:02 +0300

    Make add to cart button ineractive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -45,9 +45,34 @@ products.forEach((products) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">Add to Cart</button>
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${
+            products.id
+          }">Add to Cart</button>
         </div>`;
 });
-console.log(productsHTML);
+//console.log(productsHTML);
 
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
+
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) matchingItem.quantity++;
+    else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+
+    console.log(cart);
+  });
+});
